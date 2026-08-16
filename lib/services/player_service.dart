680541f4dc66834b06a1fb1f8cecdda9bdbccc0f
fazasn11-source <fazasn11@gmail.com>
2026-08-16 23:25:01 +0500

    Update player_service.dart

--- lib/services/player_service.dart
+++ lib/services/player_service.dart
@@ -1,1 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
+import '../models/song.dart';
 
+class PlayerService extends ChangeNotifier {
+  late AudioPlayer _audioPlayer;
+  Song? _currentSong;
+  bool _isPlaying = false;
+  Duration _position = Duration.zero;
+  Duration _duration = Duration.zero;
+
+  Song? get currentSong => _currentSong;
+  bool get isPlaying => _isPlaying;
+  Duration get position => _position;
+  Duration get duration => _duration;
+
+  PlayerService() {
+    _audioPlayer = AudioPlayer();
+    _initAudioPlayer();
+  }
+
+  void _initAudioPlayer() {
+    _audioPlayer.positionStream.listen((position) {
+      _position = position;
+      notifyListeners();
+    });
+
+    _audioPlayer.durationStream.listen((duration) {
+      _duration = duration ?? Duration.zero;
+      notifyListeners();
+    });
+
+    _audioPlayer.playerStateStream.listen((state) {
+      _isPlaying = state.playing;
+      notifyListeners();
+    });
+
+    _audioPlayer.onPlayerStateChanged.listen((state) {
+      _isPlaying = state == PlayerState.playing;
+      notifyListeners();
+    });
+  }
+
+  Future<void> playSong(Song song) async {
+    try {
+      _currentSong = song;
+      await _audioPlayer.setUrl(song.audioUrl);
+      await _audioPlayer.play();
+      _isPlaying = true;
+      notifyListeners();
+    } catch (e) {
+      print('Error playing song: $e');
+    }
+  }
+
+  Future<void> togglePlayPause() async {
+    if (_isPlaying) {
+      await _audioPlayer.pause();
+      _isPlaying = false;
+    } else {
+      await _audioPlayer.play();
+      _isPlaying = true;
+    }
+    notifyListeners();
+  }
+
+  Future<void> seekTo(Duration position) async {
+    await _audioPlayer.seek(position);
+    notifyListeners();
+  }
+
+  Future<void> stop() async {
+    await _audioPlayer.stop();
+    _isPlaying = false;
+    _position = Duration.zero;
+    _currentSong = null;
+    notifyListeners();
+  }
+
+  @override
+  void dispose() {
+    _audioPlayer.dispose();
+    super.dispose();
+  }
+}
